5cc6c95c1207fa3b2a24d1520c9d3ea221603076
nmastroianni <suancho@gmail.com>
2021-09-03 09:02:45 -0400

    reenabling previews

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
-// import * as React from 'react'
-// import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
+import * as React from 'react'
+import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
 
-// import 'gatsby-plugin-prismic-previews/dist/styles.css'
+import 'gatsby-plugin-prismic-previews/dist/styles.css'
 
-// import './src/stylesheets/main.scss'
+import './src/stylesheets/main.scss'
 
-// export const wrapRootElement = ({ element }) => (
-//   <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
-// )
+export const wrapRootElement = ({ element }) => (
+  <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
+)
